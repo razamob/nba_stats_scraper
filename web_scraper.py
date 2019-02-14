a383100b8ee661e0cb396022ee780da6d7d4ce58
razamob <razamob@sheridancollege.ca>
2019-02-14 14:43:54 -0500

    Updated rows to show stats in 1 row per player.

--- web_scraper.py
+++ web_scraper.py
@@ -3,6 +3,7 @@ import requests
 from selenium import webdriver
 from csv import DictWriter
 import csv
+import pandas as pd
 
 driver = webdriver.Chrome('C:/Users/Mobeen/Desktop/Programming Courses/Python-Courses/PythonBootcamp/chromedriver.exe')
 driver.get("https://stats.nba.com/leaders/")
@@ -34,15 +35,18 @@ for stat in half_list:
     cater = dict((x, y) for x, y in mapped)
     nba_players.update({stat.get_text(): cater})
     statistical.clear()
+print(len(nba_players.values()))
 
 with open("nba_players.csv", "w") as file:
-    headers = ['Player', 'Category', 'Stat']
-    csv_writer = csv.writer(file)
-    csv_writer.writerow(headers)
-    for player in nba_players:
-        for caters in nba_players[player]:
-            csv_writer.writerow([player, caters, nba_players[player][caters]])
+    headers = cat_list
+    with open("nba_players.csv", "w") as file:
+        csv_writer = DictWriter(file, fieldnames=headers)
+        csv_writer.writeheader()
+        for player in nba_players:
+            output = nba_players[player]
+            csv_writer.writerow(nba_players[player])
 
+data = pd.read_csv("nba_players.csv")
+data.insert(0, "Player", [player for player in nba_players])
+data.to_csv("nba_stats.csv", index=False)
 ### GET EACH PLAYER ###
-
-
